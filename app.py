91152738222939da509a11b835fa156d38b43e60
Lynn Lin <lynn.lin@raytheon.com>
2021-10-06 20:55:23 -0400

    DSO-4890: Removed unused import

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from sqlalchemy.orm.exc import NoResultFound
 
 # Init app
 app = Flask(__name__)
